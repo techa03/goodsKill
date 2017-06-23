17a47731c82b25c15e6541abb23577ac0e701a91
heng <techa@foxmail.com>
2017-06-24 05:19:37 +0800

    sonar bugs fix

--- goodsKill-dao/src/main/java/org/seckill/common/util/AESUtil.java
+++ goodsKill-dao/src/main/java/org/seckill/common/util/AESUtil.java
@@ -1,5 +1,6 @@
 package org.seckill.common.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sun.misc.BASE64Decoder;
@@ -17,9 +18,10 @@ import java.security.SecureRandom;
  * AES加解密工具类
  * Created by shuzheng on 2017/2/5.
  */
+@Slf4j
 public class AESUtil {
     private static Logger logger= LoggerFactory.getLogger(AESUtil.class);
-    private static final String encodeRules = "zheng";
+    private static final String ENCODE_RULES = "zheng";
 
     /**
      * 加密
@@ -37,12 +39,12 @@ public class AESUtil {
             //2.根据ecnodeRules规则初始化密钥生成器
             //生成一个128位的随机源,根据传入的字节数组
             SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-            random.setSeed(encodeRules.getBytes());
+            random.setSeed(ENCODE_RULES.getBytes());
             keygen.init(128, random);
             //3.产生原始对称密钥
-            SecretKey original_key = keygen.generateKey();
+            SecretKey originalKey = keygen.generateKey();
             //4.获得原始对称密钥的字节数组
-            byte[] raw = original_key.getEncoded();
+            byte[] raw = originalKey.getEncoded();
             //5.根据字节数组生成AES密钥
             SecretKey key = new SecretKeySpec(raw, "AES");
             //6.根据指定算法AES自成密码器
@@ -50,16 +52,14 @@ public class AESUtil {
             //7.初始化密码器，第一个参数为加密(Encrypt_mode)或者解密解密(Decrypt_mode)操作，第二个参数为使用的KEY
             cipher.init(Cipher.ENCRYPT_MODE, key);
             //8.获取加密内容的字节数组(这里要设置为utf-8)不然内容中如果有中文和英文混合中文就会解密为乱码
-            byte[] byte_encode = content.getBytes("utf-8");
+            byte[] byteEncode = content.getBytes("utf-8");
             //9.根据密码器的初始化方式--加密：将数据加密
-            byte[] byte_AES = cipher.doFinal(byte_encode);
+            byte[] byteAES = cipher.doFinal(byteEncode);
             //10.将加密后的数据转换为字符串
             //这里用Base64Encoder中会找不到包
             //解决办法：
             //在项目的Build path中先移除JRE System Library，再添加库JRE System Library，重新编译后就一切正常了。
-            String AES_encode = new String(new BASE64Encoder().encode(byte_AES));
-            //11.将字符串返回
-            return AES_encode;
+            return String.valueOf(new BASE64Encoder().encode(byteAES));
         } catch (NoSuchAlgorithmException e) {
             logger.error("",e);
         } catch (NoSuchPaddingException e) {
@@ -91,12 +91,12 @@ public class AESUtil {
             //2.根据ecnodeRules规则初始化密钥生成器
             //生成一个128位的随机源,根据传入的字节数组
             SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-            random.setSeed(encodeRules.getBytes());
+            random.setSeed(ENCODE_RULES.getBytes());
             keygen.init(128, random);
             //3.产生原始对称密钥
-            SecretKey original_key = keygen.generateKey();
+            SecretKey originalKey = keygen.generateKey();
             //4.获得原始对称密钥的字节数组
-            byte[] raw = original_key.getEncoded();
+            byte[] raw = originalKey.getEncoded();
             //5.根据字节数组生成AES密钥
             SecretKey key = new SecretKeySpec(raw, "AES");
             //6.根据指定算法AES自成密码器
@@ -104,13 +104,13 @@ public class AESUtil {
             //7.初始化密码器，第一个参数为加密(Encrypt_mode)或者解密(Decrypt_mode)操作，第二个参数为使用的KEY
             cipher.init(Cipher.DECRYPT_MODE, key);
             //8.将加密并编码后的内容解码成字节数组
-            byte[] byte_content = new BASE64Decoder().decodeBuffer(content);
+            byte[] byteContent = new BASE64Decoder().decodeBuffer(content);
             /*
              * 解密
              */
-            byte[] byte_decode = cipher.doFinal(byte_content);
-            String AES_decode = new String(byte_decode, "utf-8");
-            return AES_decode;
+            byte[] byteDecode = cipher.doFinal(byteContent);
+            String aesDecode = new String(byteDecode, "utf-8");
+            return aesDecode;
         } catch (NoSuchAlgorithmException e) {
             logger.error("",e);
         } catch (NoSuchPaddingException e) {
@@ -132,13 +132,13 @@ public class AESUtil {
         String[] keys = {
                 "", "275688"
         };
-        System.out.println("key | AESEncode | AESDecode");
+        log.info("key | AESEncode | AESDecode");
         for (String key : keys) {
-            System.out.print(key + " | ");
+            log.info(key + " | ");
             String encryptString = AESEncode(key);
-            System.out.print(encryptString + " | ");
+            log.info(encryptString + " | ");
             String decryptString = AESDecode(encryptString);
-            System.out.println(decryptString);
+            log.info(decryptString);
         }
     }
 
