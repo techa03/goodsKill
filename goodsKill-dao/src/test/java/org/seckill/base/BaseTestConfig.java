6a05c50be2367b11488659c7decd3973d0033fdd
heng <techa@foxmail.com>
2017-06-24 17:36:48 +0800

    添加测试用例

--- goodsKill-dao/src/test/java/org/seckill/base/BaseTestConfig.java
+++ goodsKill-dao/src/test/java/org/seckill/base/BaseTestConfig.java
@@ -1,5 +1,6 @@
 package org.seckill.base;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,4 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class BaseTestConfig {
 
+    @Test
+    public void testSelectByPrimaryKey() throws Exception {
+
+    }
 }
