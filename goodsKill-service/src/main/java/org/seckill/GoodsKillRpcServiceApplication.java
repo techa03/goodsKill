970be50b1b653f5e791dd6a16a5784e46e2d39dd
heng <techa@foxmail.com>
2017-06-24 16:28:10 +0800

    优化gradle依赖配置

--- goodsKill-service/src/main/java/org/seckill/GoodsKillRpcServiceApplication.java
+++ goodsKill-service/src/main/java/org/seckill/GoodsKillRpcServiceApplication.java
@@ -11,15 +11,15 @@ import java.io.IOException;
  * Created by ZhangShuzheng on 2017/2/3.
  */
 public class GoodsKillRpcServiceApplication {
-    private static Logger _log = LoggerFactory.getLogger(GoodsKillRpcServiceApplication.class);
+    private static Logger logger = LoggerFactory.getLogger(GoodsKillRpcServiceApplication.class);
 
     public static void main(String[] args) throws IOException {
-        _log.info(">>>>> goodsKill-rpc-service 正在启动 <<<<<");
+        logger.info(">>>>> goodsKill-rpc-service 正在启动 <<<<<");
         ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext(
                 new String[]{"classpath:META-INF/spring/spring-*.xml", "classpath*:junit/spring/spring-dao.xml"});
         context.start();
         System.in.read();
-        _log.info(">>>>> goodsKill-rpc-service 启动完成 <<<<<");
+        logger.info(">>>>> goodsKill-rpc-service 启动完成 <<<<<");
     }
 
 }
