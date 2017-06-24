be1e84bc60a3689d025c7a45b6f4d660623dfd84
heng <techa@foxmail.com>
2017-06-24 17:32:34 +0800

    添加测试用例

---
+++ goodsKill-dao/src/test/java/org/seckill/base/BaseTestConfig.java
@@ -0,0 +1,16 @@
+package org.seckill.base;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by heng on 2017/6/24.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:junit/spring/spring-dao.xml"})
+@Transactional
+public class BaseTestConfig {
+
+}
