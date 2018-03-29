90d60cea63a3fe9ea7c1333613e3f9b292d6626d
heng <275688448@qq.com>
2018-03-29 23:18:17 +0800

    完善错误页面跳转，发生错误可跳转至相应错误页面

---
+++ goodsKill-web/src/main/webapp/html/js/custom.js
@@ -0,0 +1,39 @@
+﻿/*====================================
+ Free To Use For Personal And Commercial Usage
+Author: #
+ License: Open source - MIT
+ Please visit http://opensource.org/licenses/MIT for more Full Deatils of license.
+ Share Us if You Like our work 
+ Enjoy Our Codes For Free always.
+======================================*/
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+           
+            var count = new countUp("error-link", 10, 404, 0, 0.3); //CHANGE 404 TO THE ERROR VALUE AS YOU WANT
+
+            window.onload = function () {
+                        count.start();
+            }
+
+            /*====================================
+            WRITE YOUR SCRIPTS HERE
+            ======================================*/
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
