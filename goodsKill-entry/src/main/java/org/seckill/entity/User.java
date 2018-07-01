053d013c69103e1e5d834c8f6fcd5dfba4fece34
heng <275688448@qq.com>
2018-07-01 21:15:30 +0800

    实现Serializable接口

--- goodsKill-entry/src/main/java/org/seckill/entity/User.java
+++ goodsKill-entry/src/main/java/org/seckill/entity/User.java
@@ -1,8 +1,10 @@
 package org.seckill.entity;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class User {
+public class User implements Serializable{
+
     private Integer id;
 
     private String account;
