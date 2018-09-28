39051418ba2f51064d578266e1e892043887ac34
heng <275688448@qq.com>
2018-09-28 20:35:18 +0800

    增加数据库读写分离，AOP实现动态数据源

--- goodsKill-dao/src/main/java/org/seckill/dao/GoodsMapper.java
+++ goodsKill-dao/src/main/java/org/seckill/dao/GoodsMapper.java
@@ -1,5 +1,6 @@
 package org.seckill.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.seckill.entity.Goods;
 import org.seckill.entity.GoodsExample;
 
@@ -8,10 +9,28 @@ import java.util.List;
 /**
  * @author heng
  */
-public interface GoodsMapper extends BaseMapper<GoodsExample, Goods> {
+public interface GoodsMapper {
 
-    List<Goods> selectByExampleWithBLOBs(GoodsExample example);
+    long countByExample(GoodsExample example);
 
-    int updateByPrimaryKeyWithBLOBs(Goods record);
+    int deleteByExample(GoodsExample example);
+
+    int deleteByPrimaryKey(Integer goodsId);
+
+    int insert(Goods record);
+
+    int insertSelective(Goods record);
+
+    List<Goods> selectByExample(GoodsExample example);
+
+    Goods selectByPrimaryKey(Integer goodsId);
+
+    int updateByExampleSelective(@Param("record") Goods record, @Param("example") GoodsExample example);
+
+    int updateByExample(@Param("record") Goods record, @Param("example") GoodsExample example);
+
+    int updateByPrimaryKeySelective(Goods record);
+
+    int updateByPrimaryKey(Goods record);
 
 }
---
+++ goodsKill-dao/src/main/java/org/seckill/dao/common/DynamicDataSource.java
@@ -0,0 +1,35 @@
+package org.seckill.dao.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+@Slf4j
+public class DynamicDataSource extends AbstractRoutingDataSource {
+
+    private static ThreadLocal<String> DATASOURCE_CONTEXT = new ThreadLocal<>();
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        String key = getDatasourceContext().get();
+        if (key == null) {
+            setDatasourceContext(DataSourceEnum.MASTER.getName());
+        } else {
+            log.debug("thread:{},determine,dataSource:{}", Thread.currentThread().getName(), key);
+            return key;
+        }
+        log.debug("thread:{},determine,dataSource:{}", Thread.currentThread().getName(), DataSourceEnum.MASTER.getName());
+        return DataSourceEnum.MASTER.getName();
+    }
+
+    public static ThreadLocal<String> getDatasourceContext() {
+        return DATASOURCE_CONTEXT;
+    }
+
+    public static void setDatasourceContext(String dataSourceKey) {
+        DATASOURCE_CONTEXT.set(dataSourceKey);
+    }
+
+    public static void remove() {
+        DATASOURCE_CONTEXT.remove();
+    }
+}
