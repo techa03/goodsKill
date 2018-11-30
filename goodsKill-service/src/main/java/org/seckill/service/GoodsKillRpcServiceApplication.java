055ff5c15b4be7ec22f85ba1a660e98ecbce2cc2
heng <techa@foxmail.com>
2018-11-30 22:01:55 +0800

    pom配置更新

--- goodsKill-service/src/main/java/org/seckill/service/GoodsKillRpcServiceApplication.java
+++ goodsKill-service/src/main/java/org/seckill/service/GoodsKillRpcServiceApplication.java
@@ -1,24 +1,19 @@
 package org.seckill.service;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * 服务启动类
  * Created by techa03 on 2017/2/3.
  */
-@Slf4j
+@SpringBootApplication
+@ImportResource(value = {"classpath*:META-INF/spring/spring-*.xml"})
 public class GoodsKillRpcServiceApplication {
 
     public static void main(String[] args) {
-        log.info(">>>>> goodsKill-rpc-service 正在启动 <<<<<");
-        AbstractApplicationContext context= new ClassPathXmlApplicationContext(
-                "classpath*:META-INF/spring/spring-*.xml");
-        // 程序退出前优雅关闭JVM
-        context.registerShutdownHook();
-        context.start();
-        log.info(">>>>> goodsKill-rpc-service 启动完成 <<<<<");
+        SpringApplication.run(GoodsKillRpcServiceApplication.class);
     }
 
 }
