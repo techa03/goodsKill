7e62074258a08afd57fe824395499a13e08c0ba9
heng <275688448@qq.com>
2018-12-12 22:36:14 +0800

    优化zookeeper分布式锁方案，获取锁时间延长，防止出现大量获取锁异常信息

--- goodsKill-service/src/main/java/org/seckill/service/util/ZookeeperLockUtil.java
+++ goodsKill-service/src/main/java/org/seckill/service/util/ZookeeperLockUtil.java
@@ -9,8 +9,8 @@ import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -22,10 +22,11 @@ public class ZookeeperLockUtil implements InitializingBean {
     private ThreadLocal<Map<Long, InterProcessMutex>> threadLock = new ThreadLocal<>();
 
     public boolean lock(long seckillId) {
+
         try {
             Map<Long, InterProcessMutex> map;
             if (threadLock.get() == null) {
-                map = new HashMap();
+                map = new ConcurrentHashMap();
                 map.put(seckillId, new InterProcessMutex(client, ROOT_LOCK_PATH + "/" + String.valueOf(seckillId)));
                 threadLock.set(map);
             } else {
@@ -33,8 +34,8 @@ public class ZookeeperLockUtil implements InitializingBean {
                     map = threadLock.get();
                     map.put(seckillId, new InterProcessMutex(client, ROOT_LOCK_PATH + "/" + String.valueOf(seckillId)));
                 }
-                threadLock.get().get(seckillId).acquire(2L, TimeUnit.SECONDS);
             }
+            threadLock.get().get(seckillId).acquire(5000L, TimeUnit.MILLISECONDS);
             return true;
         } catch (Exception e) {
             log.error(e.getMessage(), e);
@@ -58,4 +59,5 @@ public class ZookeeperLockUtil implements InitializingBean {
         this.client = CuratorFrameworkFactory.newClient(PropertiesUtil.getProperty("zookeeper_ip"), retryPolicy);
         client.start();
     }
+
 }
