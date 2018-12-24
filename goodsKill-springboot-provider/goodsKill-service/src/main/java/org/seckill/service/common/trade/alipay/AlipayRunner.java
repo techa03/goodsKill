9375e40833f2fedf93c0a4373cb7cef871a5a3ad
heng <275688448@qq.com>
2018-12-24 22:05:21 +0800

    add zfbinfo

--- goodsKill-springboot-provider/goodsKill-service/src/main/java/org/seckill/service/common/trade/alipay/AlipayRunner.java
+++ goodsKill-springboot-provider/goodsKill-service/src/main/java/org/seckill/service/common/trade/alipay/AlipayRunner.java
@@ -41,7 +41,7 @@ public class AlipayRunner {
         /** 一定要在创建AlipayTradeService之前调用Configs.init()设置默认参数
          *  Configs会读取classpath下的zfbinfo.properties文件配置信息，如果找不到该文件则确认该文件是否在classpath目录
          */
-        Configs.init("zfbinfo.properties");
+        Configs.init("zfbinfo-test.properties");
 
         /** 使用Configs提供的默认参数
          *  AlipayTradeService可以使用单例或者为静态成员对象，不需要反复new
