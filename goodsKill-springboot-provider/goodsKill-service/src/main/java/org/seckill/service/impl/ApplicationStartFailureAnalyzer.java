b2032b3d4610e0f0cefc3aef1cc950219aa1ad62
heng <275688448@qq.com>
2019-03-24 17:35:48 +0800

    启动报错友好提示

---
+++ goodsKill-springboot-provider/goodsKill-service/src/main/java/org/seckill/service/impl/ApplicationStartFailureAnalyzer.java
@@ -0,0 +1,18 @@
+package org.seckill.service.impl;
+
+import org.springframework.boot.diagnostics.AbstractFailureAnalyzer;
+import org.springframework.boot.diagnostics.FailureAnalysis;
+
+/**
+ * 启动报错友好提示
+ *
+ * @author techa03
+ * @date 2019/3/24
+ */
+public class ApplicationStartFailureAnalyzer extends AbstractFailureAnalyzer {
+    @Override
+    protected FailureAnalysis analyze(Throwable rootFailure, Throwable cause) {
+        return new FailureAnalysis(cause.getMessage(), "亲，请检查配置！mongo/zookeeper/redis/kafka/zookeeper/mysql8.0/activemq都装好了吗？如有问题请参考README文档！", cause);
+    }
+
+}
