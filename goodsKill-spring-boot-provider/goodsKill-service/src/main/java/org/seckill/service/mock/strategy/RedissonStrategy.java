aa014a10ac32949dea8e50db78f1bcf579f4078b
heng <techa@foxmail.com>
2021-01-21 18:32:11 +0800

    [自动装配]增加redisson自动装配功能

--- goodsKill-spring-boot-provider/goodsKill-service/src/main/java/org/seckill/service/mock/strategy/RedissonStrategy.java
+++ goodsKill-spring-boot-provider/goodsKill-service/src/main/java/org/seckill/service/mock/strategy/RedissonStrategy.java
@@ -1,15 +1,11 @@
 package org.seckill.service.mock.strategy;
 
 import lombok.extern.slf4j.Slf4j;
-import org.redisson.Redisson;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
 import org.seckill.api.dto.SeckillMockRequestDto;
 import org.seckill.service.inner.SeckillExecutor;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import static org.seckill.api.enums.SeckillSolutionEnum.REDISSION_LOCK;
@@ -20,10 +16,9 @@ import static org.seckill.api.enums.SeckillSolutionEnum.REDISSION_LOCK;
  */
 @Component
 @Slf4j
-public class RedissonStrategy implements GoodsKillStrategy, InitializingBean {
+public class RedissonStrategy implements GoodsKillStrategy {
+    @Autowired
     private RedissonClient redissonClient;
-    @Value("${cache_ip_address}")
-    private String cacheIpAddress;
     @Autowired
     private SeckillExecutor seckillExecutor;
 
@@ -39,10 +34,4 @@ public class RedissonStrategy implements GoodsKillStrategy, InitializingBean {
         }
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        Config config = new Config();
-        config.useSingleServer().setAddress(cacheIpAddress);
-        redissonClient = Redisson.create(config);
-    }
 }
---
+++ goodskill-spring-boot-starter/src/main/java/com/goodskill/autoconfigure/cache/GkRedissonAutoConfiguration.java
@@ -0,0 +1,34 @@
+package com.goodskill.autoconfigure.cache;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redisson分布式锁自动配置
+ *
+ * @author techa03
+ * @since 2021/1/21
+ */
+@ConditionalOnClass(RedissonClient.class)
+@Configuration(proxyBeanMethods = false)
+public class GkRedissonAutoConfiguration {
+
+    @Bean
+    @ConditionalOnProperty(prefix = "spring.redis", name = {"host", "port"},
+            matchIfMissing = true)
+    @ConditionalOnMissingBean(name = "redissonClient")
+    public RedissonClient redissonClient(RedisProperties redisProperties) {
+        Config config = new Config();
+        String host = redisProperties.getHost().isBlank() ? "127.0.0.1" : redisProperties.getHost();
+        int post = redisProperties.getPort() == 0 ? 6379 : redisProperties.getPort();
+        config.useSingleServer().setAddress("redis://" + host + ":" + post);
+        return Redisson.create(config);
+    }
+}
