abec90c5f6c507ec9e5ac583d35adb4984aeb187
techa <techa@foxmail.com>
2021-02-06 20:40:26 +0800

    [重构]使用function定义消息消费者

--- goodsKill-mongo-provider/goodskill-mongo-service/src/main/java/com/goodskill/mongo/MongoReactiveApplication.java
+++ goodsKill-mongo-provider/goodskill-mongo-service/src/main/java/com/goodskill/mongo/MongoReactiveApplication.java
@@ -1,22 +1,45 @@
 package com.goodskill.mongo;
 
-import com.goodskill.mongo.topic.SeckillMockSaveTopic;
+import com.goodskill.mongo.api.SuccessKilledMongoService;
+import com.goodskill.mongo.entity.SuccessKilledDto;
+import com.goodskill.mongo.vo.SeckillMockSaveVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.function.Consumer;
 
 /**
  *
  * @author heng
  */
 @SpringBootApplication
-@EnableBinding(value = {SeckillMockSaveTopic.class, Sink.class, Source.class})
 public class MongoReactiveApplication {
+
+    @Autowired
+    private SuccessKilledMongoService successKilledMongoService;
 
     public static void main(String[] args) {
         SpringApplication.run(MongoReactiveApplication.class, args);
+    }
+
+    /**
+     * 监听seckillMongoSave队列消息
+     *
+     * @return
+     */
+    @Bean
+    public Consumer<SeckillMockSaveVo> seckillMongoSave() {
+        return person -> {
+            SuccessKilledDto successKilledDto = new SuccessKilledDto();
+            successKilledDto.setSeckillId(BigInteger.valueOf(person.getSeckillId()));
+            successKilledDto.setUserPhone(person.getUserPhone());
+            successKilledDto.setCreateTime(new Date());
+            successKilledMongoService.saveRecord(successKilledDto);
+        };
     }
 
 }
