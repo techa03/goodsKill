ed2f021a61305174c0cea21bb9ce84d22988fb47
heng <techa@foxmail.com>
2021-12-04 17:09:52 +0800

    [升级]SpringBoot以及Cloud版本提升

--- goodskill-web/src/main/java/org/seckill/web/SampleWebJspApplication.java
+++ goodskill-web/src/main/java/org/seckill/web/SampleWebJspApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 
 /**
@@ -21,7 +22,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableDiscoveryClient
 @EnableFeignClients(value = {"com.goodskill.*.api", "org.seckill.api"})
 @EnableRedisHttpSession
-//@EnableOpenApi
+@EnableOpenApi
 public class SampleWebJspApplication extends SpringBootServletInitializer {
 
     @Override
