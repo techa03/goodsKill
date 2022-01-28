2236fece9526c6172a6f87a86cce37229d9b1406
heng <techa@foxmail.com>
2022-01-28 17:58:03 +0800

    [更新]补充单元测试

--- goodskill-spring-boot-provider/goodskill-service/src/test/java/com/goodskill/service/common/trade/alipay/ZxingUtilsTest.java
+++ goodskill-spring-boot-provider/goodskill-service/src/test/java/com/goodskill/service/common/trade/alipay/ZxingUtilsTest.java
@@ -2,6 +2,7 @@ package com.goodskill.service.common.trade.alipay;
 
 import cn.hutool.core.io.FileUtil;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 
@@ -9,9 +10,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ZxingUtilsTest {
 
+    @TempDir
+    private File file;
+
     @Test
     void getQRCodeImge() {
-        String imgPath = "/qr-s.png";
+        String imgPath = file.getPath() + "/qr-s.png";
         File qrCodeImge = ZxingUtils.getQRCodeImge("http://www.goodskill.com", 100, 100, imgPath);
         FileUtil.del(qrCodeImge);
         assertNotNull(qrCodeImge);
