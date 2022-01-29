7cab919e031552e04bfe0058a4b1454573771f9e
heng <techa@foxmail.com>
2022-01-29 17:36:29 +0800

    [优化]OAuth2.0项目依赖，简化配置

--- goodskill-spring-boot-provider/goodskill-mp-dao/src/test/java/com/goodskill/mp/dao/mapper/GoodsMapperTest.java
+++ goodskill-spring-boot-provider/goodskill-mp-dao/src/test/java/com/goodskill/mp/dao/mapper/GoodsMapperTest.java
@@ -2,15 +2,12 @@ package com.goodskill.mp.dao.mapper;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.goodskill.mp.dao.Application;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class GoodsMapperTest {
     @Autowired
--- goodskill-spring-boot-provider/goodskill-mp-dao/src/test/java/com/goodskill/mp/dao/mapper/SeckillMapperTest.java
+++ goodskill-spring-boot-provider/goodskill-mp-dao/src/test/java/com/goodskill/mp/dao/mapper/SeckillMapperTest.java
@@ -1,18 +1,16 @@
 package com.goodskill.mp.dao.mapper;
 
 import com.goodskill.mp.dao.Application;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest(classes = Application.class)
 @Transactional
 public class SeckillMapperTest {
