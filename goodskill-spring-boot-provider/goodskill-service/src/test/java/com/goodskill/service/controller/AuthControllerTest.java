e629f7b5c8313c63d23d84c141c22a226d62ed2c
zheng <techa@foxmail.com>
2022-05-14 21:59:45 +0800

    Dev (#148)
    * [更新]单元测试补充
    
    * [新增]canal场景
    
    * [更新]ci配置
    
    * [新增]canal场景
    
    * [更新]单元测试补充
    
    * [更新]单元测试补充
    
    * [更新]README勘误

---
+++ goodskill-spring-boot-provider/goodskill-service/src/test/java/com/goodskill/service/controller/AuthControllerTest.java
@@ -0,0 +1,81 @@
+package com.goodskill.service.controller;
+
+import com.goodskill.api.dto.AuthResponseDTO;
+import com.goodskill.common.util.JwtUtils;
+import com.goodskill.entity.User;
+import com.goodskill.service.common.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.HashMap;
+
+import static org.mockito.Mockito.*;
+
+class AuthControllerTest {
+    @Mock
+    UserService userService;
+    @Mock
+    StringRedisTemplate redisTemplate;
+    @Mock
+    Logger log;
+    @InjectMocks
+    AuthController authController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testToken() {
+        User value = new User();
+        value.setUsername("test");
+        when(userService.getOne(any())).thenReturn(value);
+        ValueOperations<String, String> operations = mock(ValueOperations.class);
+        when(redisTemplate.opsForValue()).thenReturn(operations);
+        AuthResponseDTO result = authController.token("test", "password");
+        Assertions.assertEquals(result.getCode(), "200");
+    }
+
+    @Test
+    void testVerifyUser() {
+        String token = JwtUtils.createToken(new HashMap<>());
+        AuthResponseDTO result = authController.verifyUser(token, "userName");
+        Assertions.assertEquals(new AuthResponseDTO(token, null, null, "500", null), result);
+    }
+
+    @Test
+    void testVerifyUserExists() {
+        HashMap<String, Object> claimMap = new HashMap<>();
+        claimMap.put("username", "test");
+        String token = JwtUtils.createToken(claimMap);
+        AuthResponseDTO result = authController.verifyUser(token, "test");
+        Assertions.assertEquals(new AuthResponseDTO(token, null, "test", "200", null), result);
+    }
+
+    @Test
+    void testVerifyToken() {
+        String token = JwtUtils.createToken(new HashMap<>());
+        AuthResponseDTO result = authController.verifyToken(token);
+        Assertions.assertEquals(result.getCode(), "200");
+    }
+
+    @Test
+    void testRefresh() {
+        ValueOperations<String, String> operations = mock(ValueOperations.class);
+        when(redisTemplate.opsForValue()).thenReturn(operations);
+
+        String token = JwtUtils.createToken(new HashMap<>());
+        AuthResponseDTO r = authController.verifyToken(token);
+        when(operations.get(r.getRefreshKey())).thenReturn(JwtUtils.createToken(new HashMap<>()));
+        AuthResponseDTO result = authController.refresh(r.getRefreshKey());
+        Assertions.assertNotEquals(token, result.getToken());
+    }
+}
