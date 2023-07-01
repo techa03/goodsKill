de303aa741dd64f2e262e1515452a68542bd2fd3
heng <heng@qq.com>
2023-07-01 22:35:58 +0800

    [升级]nacos镜像升级至2.2.3 [升级]Dockerfile镜像jdk版本至17

--- goodskill-web/src/main/java/com/goodskill/web/config/SwaggerRestApiConfig.java
+++ goodskill-web/src/main/java/com/goodskill/web/config/SwaggerRestApiConfig.java
@@ -1,49 +1,50 @@
-package com.goodskill.web.config;
-
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import org.springdoc.core.GroupedOpenApi;
-import org.springdoc.core.customizers.OpenApiCustomiser;
-import org.springdoc.core.customizers.OperationCustomizer;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-
-import static org.springdoc.core.Constants.ALL_PATTERN;
-
-/**
- * Restful API 访问路径:
- * http://IP:port/{context-path}/swagger-ui.html
- * eg:http://localhost:8080/jd-config-web/swagger-ui.html
- */
+//package com.goodskill.web.config;
+//
+//import io.swagger.v3.oas.models.info.Info;
+//import io.swagger.v3.oas.models.security.SecurityRequirement;
+//import org.springdoc.core.GroupedOpenApi;
+//import org.springdoc.core.customizers.OpenApiCustomiser;
+//import org.springdoc.core.customizers.OperationCustomizer;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//
+//import static org.springdoc.core.Constants.ALL_PATTERN;
+//
+///**
+// * Restful API 访问路径:
+// * http://IP:port/{context-path}/swagger-ui.html
+// * eg:http://localhost:8080/jd-config-web/swagger-ui.html
+// */
 //@Configuration
-public class SwaggerRestApiConfig {
-    @Bean
-    @Profile("!prod")
-    public GroupedOpenApi actuatorApi(OpenApiCustomiser actuatorOpenApiCustomiser,
-                                      OperationCustomizer actuatorCustomizer,
-                                      WebEndpointProperties endpointProperties,
-                                      @Value("${springdoc.version}") String appVersion) {
-        return GroupedOpenApi.builder()
-                .group("Actuator")
-                .pathsToMatch(endpointProperties.getBasePath()+ ALL_PATTERN)
-                .addOpenApiCustomiser(actuatorOpenApiCustomiser)
-                .addOpenApiCustomiser(openApi -> openApi.info(new Info().title("Actuator API").version(appVersion)))
-                .addOperationCustomizer(actuatorCustomizer)
-                .pathsToExclude("/health/*")
-                .build();
-    }
-
-    @Bean
-    public GroupedOpenApi usersGroup(@Value("${springdoc.version}") String appVersion) {
-        return GroupedOpenApi.builder().group("users")
-                .addOperationCustomizer((operation, handlerMethod) -> {
-                    operation.addSecurityItem(new SecurityRequirement().addList("basicScheme"));
-                    return operation;
-                })
-                .addOpenApiCustomiser(openApi -> openApi.info(new Info().title("Users API").version(appVersion)))
-                .packagesToScan("com.goodskill.web")
-                .build();
-    }
-}
+//public class SwaggerRestApiConfig {
+//    @Bean
+//    @Profile("!prod")
+//    public GroupedOpenApi actuatorApi(OpenApiCustomiser actuatorOpenApiCustomiser,
+//                                      OperationCustomizer actuatorCustomizer,
+//                                      WebEndpointProperties endpointProperties,
+//                                      @Value("${springdoc.version}") String appVersion) {
+//        return GroupedOpenApi.builder()
+//                .group("Actuator")
+//                .pathsToMatch(endpointProperties.getBasePath()+ ALL_PATTERN)
+//                .addOpenApiCustomiser(actuatorOpenApiCustomiser)
+//                .addOpenApiCustomiser(openApi -> openApi.info(new Info().title("Actuator API").version(appVersion)))
+//                .addOperationCustomizer(actuatorCustomizer)
+//                .pathsToExclude("/health/*")
+//                .build();
+//    }
+//
+//    @Bean
+//    public GroupedOpenApi usersGroup(@Value("${springdoc.version}") String appVersion) {
+//        return GroupedOpenApi.builder().group("users")
+//                .addOperationCustomizer((operation, handlerMethod) -> {
+//                    operation.addSecurityItem(new SecurityRequirement().addList("basicScheme"));
+//                    return operation;
+//                })
+//                .addOpenApiCustomiser(openApi -> openApi.info(new Info().title("Users API").version(appVersion)))
+//                .packagesToScan("com.goodskill.web")
+//                .build();
+//    }
+//}
