a7dd29efa35cf33c16456266577ec68320e1a8eb
zheng <techa@foxmail.com>
2023-09-02 16:42:13 +0800

    [重构]auth模块重命名

---
+++ goodskill-auth/auth-service/src/main/java/com/goodskill/auth/authservice/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.goodskill.auth.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
