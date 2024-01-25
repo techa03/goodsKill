84122d99b71226af9262a091f020915d38851c1a
zheng <techa@foxmail.com>
2024-01-25 14:47:19 +0800

    [新增]状态机集成

--- goodskill-seckill-provider/goodskill-service/src/main/java/com/goodskill/service/SeckillApplication.java
+++ goodskill-seckill-provider/goodskill-service/src/main/java/com/goodskill/service/SeckillApplication.java
@@ -1,18 +1,12 @@
 package com.goodskill.service;
 
-import com.goodskill.common.core.enums.ActivityEvent;
-import com.goodskill.common.core.enums.SeckillActivityStates;
-import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.Banner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.statemachine.StateMachine;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,27 +27,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class SeckillApplication {
-    @Resource
-    private StateMachine<SeckillActivityStates, ActivityEvent> stateMachine;
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(SeckillApplication.class);
         application.setBannerMode(Banner.Mode.CONSOLE);
         application.run(args);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-//            feedMachine(stateMachine, ActivityEvent.ACTIVITY_CREATE);
-//            feedMachine(stateMachine, ActivityEvent.ACTIVITY_START);
-//            feedMachine(stateMachine, ActivityEvent.ACTIVITY_END);
-//            boolean b = feedMachine(stateMachine, ActivityEvent.ACTIVITY_RESET);
-            log.info("feedMachine");
-        };
-    }
-
-
-
 
 }
