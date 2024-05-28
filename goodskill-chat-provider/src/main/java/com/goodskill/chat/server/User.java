71c6994d18e3fe88d58eaeb632e4480b2c72d325
zheng <techa@foxmail.com>
2024-05-28 22:21:01 +0800

    [修复]build报错问题

---
+++ goodskill-chat-provider/src/main/java/com/goodskill/chat/server/User.java
@@ -0,0 +1,47 @@
+package com.goodskill.chat.server;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.goodskill.common.core.entity.BaseColEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author heng
+ * @since 2019-09-07
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class User extends BaseColEntity implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID=1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private String account;
+
+    private String password;
+
+    private String username;
+
+    private Integer locked;
+
+    private String avatar;
+
+    private Date lastLoginTime;
+
+    private String mobile;
+
+    private String emailAddr;
+
+}
