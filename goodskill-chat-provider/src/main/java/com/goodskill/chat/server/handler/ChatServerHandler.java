e79c4333e489857dc9bc4e4d29381faec2d56937
zheng <techa@foxmail.com>
2024-05-28 23:09:55 +0800

    [修复]build报错问题 (#215)

--- goodskill-chat-provider/src/main/java/com/goodskill/chat/server/handler/ChatServerHandler.java
+++ goodskill-chat-provider/src/main/java/com/goodskill/chat/server/handler/ChatServerHandler.java
@@ -3,7 +3,7 @@ package com.goodskill.chat.server.handler;
 import cn.hutool.core.bean.BeanUtil;
 import com.goodskill.chat.dto.ChatMessageDto;
 import com.goodskill.chat.server.User;
-import com.goodskill.common.util.JwtUtils;
+import com.goodskill.common.core.util.JwtUtils;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
